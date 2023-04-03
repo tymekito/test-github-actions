3dd2a0a9a85c7998e4b285d6a9e70510d13923c1
Tymek Słobodzian <tymekito@interia.eu>
2023-04-03 10:59:49 +0200

    init

---
+++ Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+public static class MathFunctions
+{
+    public static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
